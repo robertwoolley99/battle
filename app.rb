b490e4c297797dde28240937d23a18479fc0a514
Robert Woolley <rob@robertwoolley.co.uk>
2019-10-30 14:26:19 +0000

    Test environment set up.

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 # in app.#!/usr/bin/env ruby -wKU
 
 require 'sinatra/base'
+# require File.join(File.dirname(__FILE__),  'app.rb')
+# require 'capybara'
+# require 'capybara/rspec'
+# require 'rspec'
+
+
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    'Testing infrastructure working!'
   end
 
   # start the server if Ruby file executed directly
   run! if app_file == $0
 end
+
+# Capybara.app = Battle
