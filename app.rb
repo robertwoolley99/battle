cc5d21f67600a92c2e913c5397b386af85199a86
Robert Woolley <rob@robertwoolley.co.uk>
2019-10-30 15:27:03 +0000

    Working form and names.

--- app.rb
+++ app.rb
@@ -10,8 +10,15 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+  erb :index
   end
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+  erb :play
+  end
+
+
 
   # start the server if Ruby file executed directly
   run! if app_file == $0
